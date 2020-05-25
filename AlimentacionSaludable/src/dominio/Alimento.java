ee4a458adc7f26bd249f8fc90aecd3bcf27dbca5
Matias Salles <matiassalles99@hotmail.com>
2020-05-25 16:22:10 -0300

    Code smells corregidos y update en los analisis

--- AlimentacionSaludable/src/dominio/Alimento.java
+++ AlimentacionSaludable/src/dominio/Alimento.java
@@ -1,22 +1,21 @@
 package dominio;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import javax.swing.ImageIcon;
-import java.util.Currency;
 
 public final class Alimento implements Serializable {
 
     private String nombre;
     private String tipoAlimento;
-    private ArrayList<ComposicionAlimento> listaNutrientesConProporcion;
+    private List<ComposicionAlimento> listaNutrientesConProporcion;
     private ImageIcon fotoDelAlimento;
     private static final long serialVersionUID = 44L;
 
     public Alimento(String unNombre,
             String unTipoAlimento,
-            ArrayList<ComposicionAlimento> listaNutrientes,
+            List<ComposicionAlimento> listaNutrientes,
             ImageIcon foto) {
 
         this.setNombre(unNombre);
@@ -41,25 +40,17 @@ public final class Alimento implements Serializable {
         return this.tipoAlimento;
     }
 
-    public void setTipoAlimento(String TA) {
-        tipoAlimento = TA;
-        
+    public void setTipoAlimento(String tipoAlimento) {
+        this.tipoAlimento = tipoAlimento;
     }
 
-    public ArrayList<ComposicionAlimento> getListaNutrientesConProporcion() {
-        if (this.listaNutrientesConProporcion.isEmpty()) {
-            return new ArrayList<>();
-        } else {
-            return this.listaNutrientesConProporcion;
-        }
+    public List<ComposicionAlimento> getListaNutrientesConProporcion() {
+        return this.listaNutrientesConProporcion;
     }
 
-    public void setListaNutrientesConProporcion(ArrayList<ComposicionAlimento> listaNutrientes) {
-        if (listaNutrientes == null) {
-            this.listaNutrientesConProporcion = new ArrayList<>();
-        } else {
-            this.listaNutrientesConProporcion = listaNutrientes;
-        }
+    public void setListaNutrientesConProporcion(List<ComposicionAlimento> listaNutrientes) {
+        this.listaNutrientesConProporcion = listaNutrientes;
+
     }
 
     public ImageIcon getFotoDelAlimento() {
@@ -81,11 +72,12 @@ public final class Alimento implements Serializable {
 
     @Override
     public boolean equals(Object obj) {
-        if(!(obj instanceof Alimento))
+        if (!(obj instanceof Alimento)) {
             return false;
-            
+        }
+
         Alimento otroAlimento = (Alimento) obj;
-        return (this.getNombre().equals(otroAlimento.getNombre()));
+        return this.getNombre().equals(otroAlimento.getNombre());
     }
 
     @Override
@@ -94,5 +86,5 @@ public final class Alimento implements Serializable {
         hash = 41 * hash + Objects.hashCode(this.nombre);
         return hash;
     }
-    
+
 }
